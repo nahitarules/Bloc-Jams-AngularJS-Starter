16e15c1087b1e7f09e3f84d23ac121ed7a22ac5f
Nahome Gudeta <nahomegudeta@nahomes-mbp.attlocal.net>
2017-12-26 17:27:05 -0800

    services2 final

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -5,38 +5,39 @@
           var currentSong = null;
           var currentBuzzObject = null;
 
+
+          var setSong = function(song) {
+              if (currentBuzzObject) {
+                  currentBuzzObject.stop();
+                  currentSong.playing = null;
+              }
+
+              currentBuzzObject = new buzz.sound(song.audioUrl, {
+                  formats: ['mp3'],
+                  preload: true
+              });
+
+              currentSong = song;
+           };
+
           SongPlayer.play = function(song) {
             if (currentSong !== song) {
+                   setSong(song);
+                   currentBuzzObject.play();
+                   song.playing = true;
 
+                 } else if (currentSong === song) {
+                   if (currentBuzzObject.isPaused()) {
+                       currentBuzzObject.play();
+                       song.playing = true;
+                     }
+                   }
+               };
 
-                  currentBuzzObject.play();
-                  song.playing = true;
-                  setSong(song);
-       } else if (currentSong === song) {
-         if (currentBuzzObject.isPaused()) {
-             currentBuzzObject.play();
-             song.playing = true;
-         }
-     }
-     };
-        SongPlayer.pause = function(song) {
-            currentBuzzObject.pause();
-            song.playing = false;
+               SongPlayer.pause = function(song) {
+                 currentBuzzObject.pause();
+                 song.playing = false;
  };
-
-        var setSong = function(song) {
-            if (currentBuzzObject) {
-              currentBuzzObject.stop();
-              currentSong.playing = null;
-}
-
-        currentBuzzObject = new buzz.sound(song.audioUrl, {
-          formats: ['mp3'],
-          preload: true
-});
-
-      currentSong = song;
-};
 
           return SongPlayer;
      }
