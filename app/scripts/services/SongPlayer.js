7ba77f68597b3387f8c27ee4b08838134cbbe577
Nahome Gudeta <nahomegudeta@nahomes-mbp.attlocal.net>
2017-12-26 17:46:01 -0800

    services2Assignment

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -20,16 +20,19 @@
               currentSong = song;
            };
 
+           var playSong = function(song){
+             currentBuzzObject.play();
+             song.playing = true;
+           };
+
           SongPlayer.play = function(song) {
             if (currentSong !== song) {
                    setSong(song);
-                   currentBuzzObject.play();
-                   song.playing = true;
+                   playSong(song);
 
                  } else if (currentSong === song) {
                    if (currentBuzzObject.isPaused()) {
-                       currentBuzzObject.play();
-                       song.playing = true;
+                       playSong(song);
                      }
                    }
                };
